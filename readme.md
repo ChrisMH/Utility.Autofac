dc80ec698e42133dd3b3a77a80d3117038342aa3
Chris Hogan <chogan@logikos.com>
2011-12-19 16:33:26 -0500

    Updated module loading

--- readme.md
+++ readme.md
@@ -1,3 +1,1 @@
-#Utility
-
-General utilities that I find...useful.
+#Utility.Autofac
--- src/Utility.Autofac/ConventionContainerBuilder.cs
+++ src/Utility.Autofac/ConventionContainerBuilder.cs
@@ -3,34 +3,22 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using Autofac;
 
 namespace Utility.Autofac
 {
   /// <summary>
-  /// Builds Autofac containers by convention
+  ///   Builds Autofac containers by convention
   /// </summary>
   public static class ConventionContainerBuilder
   {
     /// <summary>
-    /// Builds an Autofac container based on discovered modules
-    /// 
-    /// The convention:
-    /// 
-    /// Any component requiring Autofac registration of components should
-    /// do that registration in a class derived from Autofac.Module.
-    /// 
-    /// This implementation will scan all assemblies in the application domain's base
-    /// directory (including subdirectories), create an instance of and register
-    /// any Autofac.IModule - derived concrete class that it finds.
+    ///   Builds an Autofac container based on discovered modules The convention: Any component requiring Autofac registration of components should do that registration in a class derived from Autofac.Module. This implementation will scan all assemblies in the application domain's base directory (including subdirectories), create an instance of and register any Autofac.IModule - derived concrete class that it finds.
     /// </summary>
-    /// <param name="excludeAutofacModules">true it exclude any IModule-derived class
-    /// defined within Autofac itself</param>
-    /// <returns>A ContainerBuilder containing all discovered module registrations</returns>
+    /// <param name="excludeAutofacModules"> true it exclude any IModule-derived class defined within Autofac itself </param>
+    /// <returns> A ContainerBuilder containing all discovered module registrations </returns>
     public static ContainerBuilder BuilderFromModules(bool excludeAutofacModules = true)
     {
-
       var autofacModuleTypes = new List<Type>();
 
       // Scan for IModule-derived concrete types
@@ -38,32 +26,28 @@ namespace Utility.Autofac
       var assemblyFileNames = di.GetFiles("*.dll", SearchOption.AllDirectories)
         .Where(fi => !(excludeAutofacModules && Path.GetFileNameWithoutExtension(fi.FullName).StartsWith("Autofac")))
         .Select(fi => fi.FullName);
-            
+
       foreach (var assemblyFileName in assemblyFileNames)
       {
-        var assembly = AppDomain.CurrentDomain.GetAssemblies().SingleOrDefault(a => a.CodeBase.Equals(assemblyFileName, StringComparison.CurrentCultureIgnoreCase));
-            
-        if(assembly == null)
+        try
         {
-          try
-          {
-            assembly = Assembly.LoadFile(assemblyFileName);
-          }
-          catch (BadImageFormatException)
-          {
-            // Not a .Net assembly
-            continue;
-          }
-        }
+          var assembly =
+            AppDomain.CurrentDomain.GetAssemblies().SingleOrDefault(a => a.CodeBase.Equals(assemblyFileName, StringComparison.CurrentCultureIgnoreCase)) ??
+            Assembly.LoadFile(assemblyFileName);
 
-        autofacModuleTypes.AddRange(assembly.GetTypes().Where(t => !t.IsAbstract && t.GetInterfaces().Contains(typeof (global::Autofac.Core.IModule))));
+          autofacModuleTypes.AddRange(assembly.GetTypes().Where(t => !t.IsAbstract && t.GetInterfaces().Contains(typeof (global::Autofac.Core.IModule))));
+        }
+        catch (BadImageFormatException)
+        {
+          // Not a .Net assembly
+        }
       }
-      
+
       // Create and register all discovered module types
       var builder = new ContainerBuilder();
       foreach (var autofacModuleType in autofacModuleTypes)
       {
-        var module = (global::Autofac.Module)autofacModuleType.Assembly.CreateInstance(autofacModuleType.FullName);
+        var module = (global::Autofac.Module) autofacModuleType.Assembly.CreateInstance(autofacModuleType.FullName);
         builder.RegisterModule(module);
       }
 
